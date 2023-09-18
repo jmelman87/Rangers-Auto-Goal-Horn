f8327025d68c91ed88cd024a03bcb4a16fc5f47a
jmelman87 <34871736+jmelman87@users.noreply.github.com>
2023-09-18 09:19:06 -0400

    Delete wtvr.py

--- wtvr.py
+++
@@ -1,92 +0,0 @@
-{
-		copyright: "NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams. © NHL 2023. All Rights Reserved.",
-		teams:  {
-		away:  {
-		team:  {
-		id: 3,
-		name: "New York Rangers",
-		link: "/api/v1/teams/3"
-		},
-		teamStats:  {
-		teamSkaterStats:  {
-		goals: 0,
-		pim: 0,
-		shots: 0,
-		powerPlayPercentage: "0.0",
-		powerPlayGoals: 0,
-		powerPlayOpportunities: 0,
-		faceOffWinPercentage: "0.0",
-		blocked: 0,
-		takeaways: 0,
-		giveaways: 0,
-		hits: 0
-		}
-		},
-		players: { },
-		goalies: [ ],
-		skaters: [ ],
-		onIce: [ ],
-		onIcePlus: [ ],
-		scratches: [ ],
-		penaltyBox: [ ],
-		coaches:  [
-		 {
-		person:  {
-		fullName: "Gerard Gallant",
-		link: "/api/v1/people/null"
-		},
-		position:  {
-		code: "HC",
-		name: "Head Coach",
-		type: "Head Coach",
-		abbreviation: "Head Coach"
-		}
-		}
-		]
-		},
-		home:  {
-		team:  {
-		id: 6,
-		name: "Boston Bruins",
-		link: "/api/v1/teams/6"
-		},
-		teamStats:  {
-		teamSkaterStats:  {
-		goals: 0,
-		pim: 0,
-		shots: 0,
-		powerPlayPercentage: "0.0",
-		powerPlayGoals: 0,
-		powerPlayOpportunities: 0,
-		faceOffWinPercentage: "0.0",
-		blocked: 0,
-		takeaways: 0,
-		giveaways: 0,
-		hits: 0
-		}
-		},
-		players: { },
-		goalies: [ ],
-		skaters: [ ],
-		onIce: [ ],
-		onIcePlus: [ ],
-		scratches: [ ],
-		penaltyBox: [ ],
-		coaches:  [
-		 {
-		person:  {
-		fullName: "Jim Montgomery",
-		link: "/api/v1/people/null"
-		},
-		position:  {
-		code: "HC",
-		name: "Head Coach",
-		type: "Head Coach",
-		abbreviation: "Head Coach"
-		}
-		}
-		]
-		}
-		},
-		officials: [ ]
-}
